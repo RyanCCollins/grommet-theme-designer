82e6ee59db11d2a420525115e7a0acaa86670716
Ryan C. Collins <rccproductions@mac.com>
2019-10-02 16:49:24 -0400

    Use HPE as base theme

--- src/theme.js
+++ src/theme.js
@@ -1,16 +1,43 @@
 
+// (C) Copyright 2017-2019 Hewlett Packard Enterprise Development LP
+import { hpe } from 'grommet-theme-hpe';
+import { deepMerge } from 'grommet/utils/object';
+import { css } from 'styled-components';
+
 export const apiUrl = 'https://us-central1-grommet-designer.cloudfunctions.net/themes';
+
+const base = deepMerge(hpe, {
+  global: {
+    size: {
+      'main-sidenav': '454px',
+      'modal-layer': '420px',
+    },
+    opacity: {
+      weak: '0.1',
+    },
+    drop: {
+      'extend': css`
+        margin-top: 10px;
+      `,
+      'border': {
+        'radius': '0px',
+      },
+    },
+  },
+  anchor: {
+    fontWeight: 'normal',
+  },
+  button: {
+    border: {
+      radius: '24px',
+    },
+  },
+});
+
 
 export const starter = {
   name: 'my theme',
-  rounding: 4,
-  spacing: 24,
-  global: {
-    colors: {},
-    font: {
-      family: 'Helvetica',
-    },
-  }
+  ...base,
 }
 
 export const upgradeTheme = (theme) => {
